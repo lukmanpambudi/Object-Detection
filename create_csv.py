0fd86762e53127de25d13aaee01811609c1617f3
lukmanpambudi <89895678+lukmanpambudi@users.noreply.github.com>
2024-01-10 10:16:06 +0700

    create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -5,31 +5,43 @@ import xml.etree.ElementTree as ET
 
 
 def xml_to_csv(path):
+    classes_names = []
     xml_list = []
+
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
+            class_name = member.find('name').text
+            classes_names.append(class_name)
+
+            xmin = int(member.find('bndbox/xmin').text)
+            ymin = int(member.find('bndbox/ymin').text)
+            xmax = int(member.find('bndbox/xmax').text)
+            ymax = int(member.find('bndbox/ymax').text)
+
             value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+                     int(root.find('size/width').text),
+                     int(root.find('size/height').text),
+                     class_name,
+                     xmin,
+                     ymin,
+                     xmax,
+                     ymax)
+
             xml_list.append(value)
+
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
-    return xml_df
+    classes_names = list(set(classes_names))
+    classes_names.sort()
+    return xml_df, classes_names
 
-
-def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
-    xml_df = xml_to_csv(image_path)
-    xml_df.to_csv('raccoon_labels.csv', index=None)
-    print('Successfully converted xml to csv.')
+for label_path in ['train_', 'validation']:
+    image_path = os.path.join(os.getcwd(), label_path)
+    xml_df, classes = xml_to_csv(label_path)
+    xml_df.to_csv(f'{label_path}.csv', index=None)
+    print(f'Successfully converted {label_path} xml to csv.')
 
 
 main()
